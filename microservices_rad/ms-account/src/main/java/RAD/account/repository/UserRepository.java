7b7708112558333f836d0aa6278851dac34cb6db
ShavitMor <102467231+ShavitMor@users.noreply.github.com>
2024-11-11 20:40:52 +0200

    update docker

--- microservices_rad/ms-account/src/main/java/RAD/account/repository/UserRepository.java
+++ microservices_rad/ms-account/src/main/java/RAD/account/repository/UserRepository.java
@@ -2,7 +2,9 @@ package RAD.account.repository;
 
 import RAD.account.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends MongoRepository<User, String> {
     User findByUsername(String username);
     Boolean existsByEmail(String email);
